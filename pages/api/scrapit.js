dab606d4d6c4d62b33c5db5175494827997a0f17
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-11-30 22:08:31 +0100

    Improving scraper

--- pages/api/scrapit.js
+++ pages/api/scrapit.js
@@ -1,48 +1,71 @@
 const { chromium } = require('playwright');
 const { compact } = require('jsonld');
 
-const scraper = async url => {
-    console.log('url: ', url);
+export default async function scraper(req, res) {
+    // console.log('url: ', url);
+    try {
+        if (req.method !== 'POST') {
+            res.status(405).send({ message: 'Only POST requests allowed' });
+            return;
+        }
 
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
+        const { url } = req?.body;
 
-    await page.goto(url);
+        console.log(url);
 
-    const handle = await page.$('script[type="application/ld+json"]');
-    const text = await handle.innerText();
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
 
-    const schema = {
-        recipe: 'http://schema.org/Recipe',
-        recipeIngredient: 'http://schema.org/recipeIngredient',
-        recipeInstructions: 'http://schema.org/recipeInstructions',
-        Text: 'http://schema.org/text',
-        HotToStep: 'http://schema.org/HowToStep',
-    };
+        await page.goto(url);
 
-    const compacted = await compact(JSON.parse(text), schema);
+        const handle = await page.$('script[type="application/ld+json"]');
+        const text = await handle.innerText();
 
-    let ingredients = compacted?.recipeIngredient;
+        const schema = {
+            recipe: 'http://schema.org/Recipe',
+            recipeIngredient: 'http://schema.org/recipeIngredient',
+            recipeInstructions: 'http://schema.org/recipeInstructions',
+            Text: 'http://schema.org/text',
+            HotToStep: 'http://schema.org/HowToStep',
+        };
 
-    if (!ingredients) {
-        ingredients = compacted['@graph'].filter(item => item['@type'] === 'recipe')[0]?.recipeIngredient;
+        const compacted = await compact(JSON.parse(text), schema);
+
+        let ingredients = compacted?.recipeIngredient;
+
+        if (!ingredients) {
+            ingredients = compacted['@graph'].filter(item => item['@type'] === 'recipe')[0]?.recipeIngredient;
+        }
+
+        let instructions = compacted?.recipeInstructions;
+
+        if (!instructions) {
+            instructions = compacted['@graph']?.filter(item => item['@type'] === 'recipe')[0]
+                ?.recipeInstructions;
+            instructions = instructions?.map(i => i.Text);
+        }
+
+        if (instructions[0]['@type'] === 'HotToStep') {
+            instructions = instructions?.map(i => i.Text);
+        }
+
+        // console.log(compacted);
+        // console.log(ingredients);
+        // console.log(instructions);
+
+        await page.close();
+        await browser.close();
+
+        // return ingredients;
+
+        if (ingredients || instructions) {
+            res.status(200).json({ items: ingredients, steps: instructions });
+        } else {
+            res.status(200).json({ message: 'No data found!' });
+        }
+    } catch (error) {
+        res.status(404).json({ message: 'Site can not be imported!' });
     }
+}
 
-    let instructions = compacted?.recipeInstructions;
-
-    if (!instructions) {
-        instructions = compacted['@graph']?.filter(item => item['@type'] === 'recipe')[0]?.recipeInstructions;
-        instructions = instructions?.map(i => i.Text);
-    }
-
-    // console.log(compacted);
-    console.log(ingredients);
-    console.log(instructions);
-
-    await page.close();
-    await browser.close();
-
-    return ingredients;
-};
-
-scraper('https://www.allrecipes.com/recipe/10275/classic-peanut-butter-cookies/');
+// scraper('https://www.allrecipes.com/recipe/10275/classic-peanut-butter-cookies/');

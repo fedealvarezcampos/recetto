02d9738fed59e9d511fa504585af3ca285b23b69
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-12-05 22:53:13 +0100

    Home page, general styles

--- components/layout.js
+++ components/layout.js
@@ -33,16 +33,7 @@ function Layout({ children, modal, setModal }) {
                 {modal && !session && <Login setModal={setModal} />}
                 <main className={styles.main}>{children}</main>
                 <footer className={styles.footer}>
-                    <a
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Powered by{' '}
-                        <span className={styles.logo}>
-                            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                        </span>
-                    </a>
+                    <span>Recetto © 2021</span>
                 </footer>
             </div>
         </>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,74 @@
-// import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import recipes from '../public/images/recipes.png';
+import addingRecipes from '../public/images/addrecipe.png';
 
 export default function Home() {
-    return <div>something something</div>;
+    return (
+        <>
+            <div className={styles.homeContainer}>
+                <motion.h1
+                    initial={{ y: -40, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.6 }}
+                >
+                    A place for all your recipes
+                </motion.h1>
+                <motion.p
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.6, delay: 0.3 }}
+                >
+                    Recetto keeps all your recipes together and organized.
+                    <br />
+                    Add new ones yourself or import them from other sites!
+                </motion.p>
+                <motion.div
+                    className={styles.firstInfoContainer}
+                    initial={{ x: -30, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 0.4, delay: 0.6 }}
+                >
+                    <p>
+                        Once logged in, you can add your own recipes with our form or import your favorites
+                        from many popular recipe sites.
+                    </p>
+                    <Image
+                        src={addingRecipes}
+                        placeholder="blur"
+                        priority
+                        // layout="intrinsic"
+                        // width="100%"
+                        // height="100%"
+                        objectFit="contain"
+                        quality={80}
+                        alt="recipe images"
+                    />
+                </motion.div>
+                <motion.div
+                    className={styles.firstInfoContainer}
+                    initial={{ x: -30, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 0.4, delay: 0.8 }}
+                >
+                    <p>
+                        You can check your saved recipes in your profile and display them according to their
+                        category.
+                    </p>
+                    <Image
+                        src={recipes}
+                        placeholder="blur"
+                        priority
+                        // layout="intrinsic"
+                        // width="100%"
+                        // height="100%"
+                        objectFit="contain"
+                        quality={80}
+                        alt="recipe images"
+                    />
+                </motion.div>
+            </div>
+        </>
+    );
 }
